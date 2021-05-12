83bd7ff167a41299c3af0c05ebf4af319222c565
SonYB98 <sonojong2030@naver.com>
2021-05-12 12:37:13 +0900

    nickname in

--- pages/index.vue
+++ pages/index.vue
@@ -1,73 +1,32 @@
 <template>
-  <div class="container">
+  <section class="container">
     <div>
-      <Logo />
       <h1 class="title">
-        mycloud1
+        <p>주제 정하기 </p>
+        <p>애완동물 상태체크</p>
       </h1>
+      <h2 class="subtitle">
+        
+      </h2>
       <div class="links">
         <a
-          href="https://nuxtjs.org/"
+          href="https://cyber.mokwon.ac.kr//"
           target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+          class="button--green">MokwonCybercampus</a>
+
       </div>
     </div>
-  </div>
+  </section>
 </template>
 
 <script>
-export default {}
+import AppLogo from '~/components/AppLogo.vue'
+
+export default {
+  components: {
+    AppLogo
+  }
+}
 </script>
 
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
 
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
