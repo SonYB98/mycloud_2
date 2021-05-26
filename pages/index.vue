d7fb61f56aeb92b39bedf6eeb4d6f13b46120106
SonYB98 <sonojong2030@naver.com>
2021-05-26 11:37:39 +0900

    add Bulma CSS

--- pages/index.vue
+++ pages/index.vue
@@ -3,12 +3,49 @@
     <section class="hero is-primary">
       <div class="hero-body">
         <p class="title">
-          애완동물 상태체크
+          <i class="fas fa-paw"></i> 애완동물 상태체크
+          <i class="fas fa-paw"></i>
         </p>
-        <p class="subtitle">
-          애완동물의 건강정보에 대해 볼수 있는 곳
-        </p>
+        <p class="subtitle">애완동물의 건강정보에 대해 볼수 있는 곳</p>
       </div>
     </section>
+    <div class="columns">
+      <div class="column">
+        <article class="message">
+          <div class="message-header">
+            <p>임시1(아직 못정함)</p>
+          </div>
+          <div class="message-body">
+            <figure class = "image is-128x128">
+              <img src ="/image/dog.jpg" alt="" />
+            </figure>
+            임시1에 대한 내용
+          </div>
+        </article>
+      </div>    
+
+      <div class="column">
+        <article class="message">
+          <div class="message-header">
+            <p>임시2(아직 못정함)</p>
+          </div>
+          <div class="message-body">
+            임시2에 대한 내용
+          </div>
+        </article>
+      </div>
+
+      <div class="column">
+        <article class="message">
+          <div class="message-header">
+            <p>임시3(아직 못정함)</p>
+          </div>
+          <div class="message-body">
+            임시3에 대한 내용
+          </div>
+        </article>
+      </div>
+
+    </div>
   </div>
 </template>
